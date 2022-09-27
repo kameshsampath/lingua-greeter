03ab46360a8cc7513fb9251b4da7f516ef44e0a2
Kamesh <kamesh.sampath@hotmail.com>
2022-09-27 23:23:27 +0530

    using bas64

--- scripts/rm-secrets.sh
+++ scripts/rm-secrets.sh
@@ -11,3 +11,5 @@ drone secret rm --name image_registry "${LINGUA_GREETER_GIT_REPO}"
 drone secret rm --name image_registry_user "${LINGUA_GREETER_GIT_REPO}"
 
 drone secret update --name image_registry_password "${LINGUA_GREETER_GIT_REPO}"
+
+drone secret rm --name kubeconfig_content user-01/lingua-greeter
