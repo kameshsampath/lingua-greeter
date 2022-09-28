766dd865734f2ea31ff6505b655d06b5cf1b75bc
Kamesh <kamesh.sampath@hotmail.com>
2022-09-28 14:19:18 +0530

    trigger 2

--- scripts/add-gcp-creds.sh
+++ scripts/add-gcp-creds.sh
@@ -8,4 +8,4 @@ GOOGLE_APPLICATION_CREDENTIALS="$(echo -n "${GOOGLE_APPLICATION_CREDENTIALS}" | 
 
 #echo "$GOOGLE_APPLICATION_CREDENTIALS"
 
-# drone secret add --name google_application_credentials --data "'$GOOGLE_APPLICATION_CREDENTIALS'" "${LINGUA_GREETER_GIT_REPO}"
+drone secret add --name google_application_credentials --data "'$GOOGLE_APPLICATION_CREDENTIALS'" "${LINGUA_GREETER_GIT_REPO}"
